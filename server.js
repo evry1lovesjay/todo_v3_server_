e5e8b2db71add6dedebbc8734403a106c27be1bb
evry1lovesjay <evry1lovesjay@yahoo.com>
2023-02-23 17:54:44 +0100

    added Mongodb atlas link

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.json())
 app.use(cors())
 
 
-mongoose.connect(process.env.MONGO_CONNECTION, {
+mongoose.connect("mongodb+srv://todo_v3_renderdb_user:ASc00lAS_@@cluster0.emjbcyk.mongodb.net/?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(()=> console.log("Connected to DB...")).catch(console.err)
